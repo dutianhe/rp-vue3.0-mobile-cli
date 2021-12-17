eb054eef9b479c49748245173afeaf27beac38e1
tianhe.du <dutianhe@qq.com>
2021-12-17 18:24:06 +0800

    增加vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <Home></Home>
+  <router-view />
 </template>
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Home from "./pages/Home.vue";
 
 </script>
 <style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import "amfe-flexible/index.js"
 import sa from '@/utils/saTrack';
 import service from "@/service/entry"
 import Vconsole from 'vconsole';
-
+import VueRouter from "./router"
 if (import.meta.env.VITE_MODE != 'release') {
     new Vconsole()
 }
 const app = createApp(App)
 app.provide("$service",service)
 app.provide("$sa",sa)
+app.use(VueRouter)
 app.mount('#app');
